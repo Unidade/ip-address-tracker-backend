66daf120cd6e31f9e3c2ce4d13c8e9039f1ebb78
Unidade <68622249+Unidade@users.noreply.github.com>
2022-11-22 14:44:19 -0300

    Add domain query parameter

--- app.js
+++ app.js
@@ -18,14 +18,17 @@ app.use(cors())
 app.use(express.json())
 
 app.post('/', async (req, res, next) => {
-  const { ipAddress } = req.body
-
-  if (!ipAddress) {
-    return res.status(400).send('ipAddress not found on the request')
+  const { ipAddress, domain } = req.body
+  if (!ipAddress && !domain) {
+    return res
+      .status(400)
+      .send('Neither ipAddress nor domain was found on the body request')
   }
 
   try {
-    const data = await getLocationByIpAddress(ipAddress)
+    const data = ipAddress
+      ? await getLocationByIpAddress(ipAddress)
+      : await getLocationByDomain(domain)
 
     if (data.code) {
       return res.status(400).send(data.messages)
@@ -53,6 +56,26 @@ async function getLocationByIpAddress(ipAddress) {
 
     const data = await fetch(
       `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipAddress}`,
+      requestOptions
+    )
+
+    const text = await data.json()
+
+    return text
+  } catch (e) {
+    throw e
+  }
+}
+
+async function getLocationByDomain(domain) {
+  try {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    }
+
+    const data = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&domain=${domain}`,
       requestOptions
     )
 
